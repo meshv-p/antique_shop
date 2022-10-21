9a5797962dfd5fb6942189fbb64798889bd3bfdd
Meshv Patel <meshv1823@gmail.com>
2022-10-21 21:17:35 +0530

    Search ui Upgraded

--- compo/CommandPalete.jsx
+++ compo/CommandPalete.jsx
@@ -1,36 +1,36 @@
-import { Fragment, useEffect, useState } from 'react'
+/*
+  This example requires Tailwind CSS v3.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { Fragment, useState } from 'react'
 import { Combobox, Dialog, Transition } from '@headlessui/react'
 import { SearchIcon } from '@heroicons/react/solid'
 import { ExclamationCircleIcon } from '@heroicons/react/outline'
 import { useFetch, useSearch } from '../store'
-import Image from 'next/image'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-var md = require('markdown-it')();
+import { ImageCompo, getImgUrl } from './ImageCompo';
+import Image from 'next/image'
 
 const items = [
     {
         id: 1,
-        name: 'Animal Crossing: New Horizons',
-        description: 'Work from home accessories',
-        imageSrc: '/animal.jfif',
-        imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-        href: '/product/Animal Crossing: New Horizons',
-    },
-    {
-        id: 2,
-        name: 'Clock',
-        description: 'Journals and note-taking',
-        imageSrc: '/clock.jfif',
-        imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-        href: '/product/Clock',
-    },
-    {
-        id: 3,
-        name: 'Ganesha',
-        description: 'Daily commute essentials',
-        imageSrc: '/ganesh_murti.jfif',
-        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-        href: '/product/Ganesha',
+        name: 'Sliders',
+        description: 'A collection of sliders for selecting a range of values.',
+        url: '#',
+        imageUrl: 'https://tailwindui.com/img/component-images/icon-sliders.png',
     },
     // More items...
 ]
@@ -39,42 +39,47 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function CommandPalete() {
-
-    let router = useRouter()
-
-    // let { isSearch } = props;
+export default function Example() {
     let isSearch = useSearch(state => state.isSearch);
     let closeSearch = useSearch(state => state.close);
-    let urlFetch = useFetch()
-
 
     const [query, setQuery] = useState('')
-    const [items, setItems] = useState([])
 
-    async function handleSearch() {
-        let data = await urlFetch.urlFetch(`/items?_q=${query}&populate=*`, 'GET')
-        console.log(data);
-        setItems(data.data)
-        // closeSearch()
+    const [open, setOpen] = useState(true)
+    const [items, setItems] = useState([])
+    let urlFetch = useFetch()
+    let router = useRouter()
+
+    function search(query) {
+        return urlFetch.urlFetch(`/items?_q=${query}&populate=*`, 'GET').then((data) => {
+            // console.log(data)
+            return data
+        })
     }
 
-    useEffect(() => {
-        if (query.length > 0) {
-            handleSearch()
-        }
 
 
-    }, [query])
+    // on change input value and fetch data
+    function handleChange(e) {
+        console.log(e.target.value)
+        setQuery(e.target.value)
+        search(e.target.value).then((data) => {
+            console.log(data.data)
+            setItems(data.data)
+
+        })
+
+
+        // urlFetch.urlFetch(`/items?_q=${query}&populate=*`, 'GET', false).then((data) => {
+        //     console.log(data)
+        // }
+        // )
+
+
+    }
 
 
 
-    // const filteredItems =
-    //     query === ''
-    //         ? []
-    //         : items.filter((item) => {
-    //             return item.name.toLowerCase().includes(query.toLowerCase())
-    //         })
 
     return (
         <Transition.Root show={isSearch} as={Fragment} afterLeave={() => setQuery('')}>
@@ -88,11 +93,11 @@ export default function CommandPalete() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                 >
-                    <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-50 blur-sm backdrop-blur-0 transition-opacity" />
+                    <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
                 </Transition.Child>
 
                 <Transition.Child
-                    // as={Fragment}
+                    as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0 scale-95"
                     enterTo="opacity-100 scale-100"
@@ -103,20 +108,15 @@ export default function CommandPalete() {
                     <Combobox
                         as="div"
                         className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all"
-                        onChange={(e) => {
-                            console.log(e);
-                            // return alert('hi')
-                            // router.push(item.attributes.Slug)
-                            // closeSearch()
+                        onChange={(item) => {
+                            router.push(`/product/${item.attributes.Slug}`)
+                            closeSearch()
                         }
-
-                            // (window.location = item.href)
-
                         }
-                        value={items}
-
                     >
-                        <div className="relative focus-visible:outline-none">
+
+                        {/* // (window.location = `/product/${item.attributes.Slug}`)} */}
+                        <div className="relative">
                             <SearchIcon
                                 className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
                                 aria-hidden="true"
@@ -124,16 +124,18 @@ export default function CommandPalete() {
                             <Combobox.Input
                                 className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-sm text-gray-800 placeholder-gray-400 focus:ring-purple-600 focus-visible:outline-none ring-1 ring-purple-600"
                                 placeholder="Search..."
-                                onChange={(event) => setQuery(event.target.value)}
+                                onChange={(event) => handleChange(event)}
                             />
                         </div>
 
-                        {items.length > 0 && (
+
+                        {items.length !== 0 && (
                             <Combobox.Options static className="max-h-96 scroll-py-3 overflow-y-auto p-3">
+
                                 {items.map((item) => (
                                     <Combobox.Option
-                                        key={item.idOfItem}
-                                        value={item.idOfItem}
+                                        key={item.id}
+                                        value={item}
                                         className={({ active }) =>
                                             classNames('flex cursor-default select-none rounded-xl p-3', active && 'bg-gray-100')
                                         }
@@ -147,20 +149,29 @@ export default function CommandPalete() {
                                                     )}
                                                 >
                                                     <Image
-                                                        src={`http://localhost:1337${item.attributes?.Imgs.data[0].attributes.formats.thumbnail.url}`}
+                                                        src={`http://localhost:1337${item.attributes.Imgs.data[0].attributes.formats.thumbnail.url}`}
+                                                        className=" rounded-full object-cover"
+                                                        alt=""
+                                                        height='32px' width='32px'
 
-                                                        height='32px' width='32px' alt="" className="h-8 w-8 object-cover" />
+                                                    // layout="fill"
+                                                    // alt={i.attributes.name}
+                                                    />
+                                                    {/* <img
+
+                                                        src={`https://strapi-meshv.herokuapp.com${item.attributes?.Imgs.data[0].attributes.formats.large.url}`}
+
+                                                        // src={item.attributes.imageUrl}
+
+                                                        alt="" className="h-8 w-8" /> */}
                                                 </div>
                                                 <div className="ml-4 flex-auto">
                                                     <p className={classNames('text-sm font-medium', active ? 'text-gray-900' : 'text-gray-700')}>
                                                         {item.attributes.name}
                                                     </p>
-                                                    <p className={classNames('text-sm', active ? 'text-gray-700' : 'text-gray-500')}
-                                                        dangerouslySetInnerHTML={{ __html: `${md.render((item.attributes.desc).slice(0, 20))}...` }}
-
-                                                    />
-                                                    {/* {item.attributes.desc}
-                                                    </p> */}
+                                                    <p className={classNames('text-sm', active ? 'text-gray-700' : 'text-gray-500')}>
+                                                        {(item.attributes.desc).slice(0, 10)}
+                                                    </p>
                                                 </div>
                                             </>
                                         )}
@@ -177,16 +188,12 @@ export default function CommandPalete() {
                                     className="mx-auto h-6 w-6 text-gray-400"
                                 />
                                 <p className="mt-4 font-semibold text-gray-900">No results found</p>
-                                <p className="mt-2 text-gray-500">No components found for this search term. Please try again.</p>
+                                <p className="mt-2 text-gray-500">No items found for this search term. Please try again.</p>
                             </div>
                         )}
                     </Combobox>
                 </Transition.Child>
             </Dialog>
-        </Transition.Root >
+        </Transition.Root>
     )
 }
-
-
-// {{Local_URL}}/products?_q=New
-
